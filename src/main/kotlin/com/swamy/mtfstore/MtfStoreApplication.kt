c3afed10b92c4dacca01dd124042169a39d6026b
Swamy Sivasubramaniyan <s.swamynathan@gmail.com>
2023-02-27 20:17:40 -0700

    Init commit

---
+++ src/main/kotlin/com/swamy/mtfstore/MtfStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.swamy.mtfstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MtfStoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<MtfStoreApplication>(*args)
+}
